a5f8fc8403ce9f54c389000e2ff035b8ec8e9f9f
Tim <tim.xie.2009@gmail.com>
2020-09-03 01:03:49 -0400

    mhh 68

--- webp_convert.sh
+++ webp_convert.sh
@@ -3,7 +3,7 @@ pushd $DIR
 mkdir webp
 rm webp/*.webp
 PG=0
-for f in $(ls *.png); do
+for f in $(ls -v *.png); do
 	echo $f
 	mv $f $PG.png
 	convert $PG.png -resize x1440 -filter triangle -quality 80% webp/$(echo $PG.png | sed 's/.png//').webp
